8548b46ff2319723b03fc089168431427b62078d
zoe yeh <zoeyeh@zoes-MacBook-Pro.local>
2022-05-06 00:24:19 +0800

    access google sheet

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ for row in rows:
 uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg'])
 if uploaded_file is not None:
     file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
-     st.write(file_details)
+    st.write(file_details)
 
 # # https://blog.streamlit.io/streamlit-firestore-continued/
 
