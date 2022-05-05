86b80f7b715fdd413e32d7d022edb0ce8b04cc0a
zoe yeh <zoeyeh@zoes-MacBook-Pro.local>
2022-05-06 06:23:09 +0800

    access google sheet

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,6 +41,9 @@ uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg'])
 if uploaded_file is not None:
     file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
     st.write(file_details)
+	# To View Uploaded Image
+	st.image(load_image(image_file),width=250)
+
 
 # # https://blog.streamlit.io/streamlit-firestore-continued/
 
