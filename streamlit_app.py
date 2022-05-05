c3ae900da961d3275ac04ca870f514ea0e564e35
zoe yeh <zoeyeh@zoes-MacBook-Pro.local>
2022-05-05 22:20:33 +0800

    new version

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,15 @@ from google.cloud import firestore
 # Authenticate to Firestore with the JSON account key.
 db = firestore.Client.from_service_account_json("streamlit-reddit-ebe13-firebase-adminsdk-h9zlm-e17d9202bf.json")
 
-# Create a reference to the Google post.
-doc_ref = db.collection("posts").document("Google")
+# # Create a reference to the Google post.
+# doc_ref = db.collection("posts").document("Google")
 
-# Then get the data at that reference.
-doc = doc_ref.get()
+# # Then get the data at that reference.
+# doc = doc_ref.get()
 
-# Let's see what we got!
-st.write("The id is: ", doc.id)
-st.write("The contents are: ", doc.to_dict())
+# # Let's see what we got!
+# st.write("The id is: ", doc.id)
+# st.write("The contents are: ", doc.to_dict())
 
 
 # This time, we're creating a NEW post reference for Apple
