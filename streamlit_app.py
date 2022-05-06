05b57742c0f93d1343c67b73dd6041349e0be05f
zoe yeh <zoeyeh@zoes-MacBook-Pro.local>
2022-05-06 22:00:53 +0800

    new version

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ from pydrive.drive import GoogleDrive
 # 	time.sleep(0.1)
 
 db = firestore.Client.from_service_account_json("firestore-key.json")
-st.markdown('<style>div {width: 1200px;height: 1200px;background-image: url(img/anita_test.jpeg);background-repeat: no-repeat;}</style>', unsafe_allow_html=True)
+
 # 網頁配置設定
 st.set_page_config(
 	page_title="Anita Mui 出道 40 週年應援活動", 
@@ -26,7 +26,7 @@ st.set_page_config(
 	layout="wide",  
 	initial_sidebar_state="collapsed")
 
-# st.image("./img/anita_test.jpeg", width=300)
+st.image("./img/anita_test.jpeg", width=300)
 
 
 # Perform SQL query on the Google Sheet.
@@ -64,85 +64,9 @@ def load_image(image_file):
 	img = Image.open(image_file)
 	return img
 
-# image_file = st.file_uploader("Upload Files", type=['png','jpeg'], accept_multiple_files = True)
 image_file = st.file_uploader("放張你最喜歡的 Anita 或是為今天留下一個紀念吧！", type=['png','jpeg'])
 
 if image_file is not None:
 	# To View Uploaded Image
 	st.image(load_image(image_file), width=300)
 
-
-
-# @st.cache(ttl=600)
-# def run_query(query):
-#     rows = conn.execute(query, headers=1)
-#     rows = rows.fetchall()
-#     return rows
-
-# sheet_url = st.secrets["private_gsheets_url"]
-# rows = run_query(f'SELECT * FROM "{sheet_url}"')
-
-# # Print results.
-# for row in rows:
-# 	st.text(row)
-# 	st.write(f"拜訪時間: {row.時間戳記}\n  我想說: {row.留下一句話你最想跟_Anita_說的話吧_}")
-
-# 	# st.image("https://drive.google.com/open?id=14456bhRmjPaxYPWmKw4DfjLCQ4MAQZDz") # Manually Adjust the width of the image as per requirement
-#     # st.write(f"{row._3}")
-# 	# st.write(f"{row.留下一句話你最想跟_Anita_說的話吧_}")
-
-
-
-# 	#Saving upload
-# 	with open(image_file.name,"wb") as f:
-# 		f.write((image_file).getbuffer())
-			  
-# 	st.success("File Saved")
-
-# 	folder_id = '17ltL5jMFTiQr23tVlacN6Q_4NLMBGrx7NpSOi3xYQxBi2ApBffOE1FbHgAzvHkKwk88oQJDF'
-# 	gfile = drive.CreateFile({'parents': [{'id': folder_id}]})
-# 	# Read file and set it as the content of this instance.
-# 	gfile.SetContentFile(image_file)
-# 	gfile.Upload() # Upload the file.
-
-	
-
-# 	# file_details = {"FileName":image_file.name,"FileType":image_file.type,"FileSize":image_file.size}
-#     # st.text(file_details)
-	
-	
-
-
-# # # https://blog.streamlit.io/streamlit-firestore-continued/
-
-# # import streamlit as st
-# # from google.cloud import firestore
-
-# # db = firestore.Client.from_service_account_json("firestore-key.json")
-# # # import json
-# # # key_dict = json.loads(st.secrets["textkey"])
-# # # creds = service_account.Credentials.from_service_account_info(key_dict)
-# # # db = firestore.Client(credentials=creds, project="streamlit-reddit")
-
-# # # Streamlit widgets to let a user create a new post
-# # title = st.text_input("Post title")
-# # url = st.text_input("Post url")
-# # submit = st.button("Submit new post")
-
-# # # Once the user has submitted, upload it to the database
-# # if title and url and submit:
-# # 	doc_ref = db.collection("posts").document(title)
-# # 	doc_ref.set({
-# # 		"title": title,
-# # 		"url": url
-# # 	})
-
-# # # And then render each post, using some light Markdown
-# # posts_ref = db.collection("posts")
-# # for doc in posts_ref.stream():
-# # 	post = doc.to_dict()
-# # 	title = post["title"]
-# # 	url = post["url"]
-
-# # 	st.subheader(f"Post: {title}")
-# # 	st.write(f":link: [{url}]({url})")
