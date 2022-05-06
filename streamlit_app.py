2a5ae641999de50aee56a14ca1fb144d85c050b2
zoe yeh <zoeyeh@zoes-MacBook-Pro.local>
2022-05-07 00:07:05 +0800

    new version

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,18 +23,17 @@ st.set_page_config(
 	initial_sidebar_state="collapsed")
 
 
-# 加入進度條, 增加一個空白元件，等等要放文字
-latest_iteration = st.empty()
-bar = st.progress(0)
-for i in range(100):
-	latest_iteration.text(f"目前進度: {i+1} %")
-	bar.progress(i + 1)
-	time.sleep(0.1)
+# # 加入進度條, 增加一個空白元件，等等要放文字
+# latest_iteration = st.empty()
+# bar = st.progress(0)
+# for i in range(100):
+# 	latest_iteration.text(f"目前進度: {i+1} %")
+# 	bar.progress(i + 1)
+# 	time.sleep(0.1)
 
 
 st.markdown("<h1 style='text-align: center; color: black;'>梅艷芳出道四十週年紀念晝展</h1>", unsafe_allow_html=True)
 st.markdown("<h2 style='text-align: center; color: black;'>未變情懷40載, 流年似水如一夢</h2>", unsafe_allow_html=True)
-# 
 
 # 背景圖
 def get_base64(bin_file):
