59d962b542413aabe18449aab21cda05046edf3b
zoe yeh <zoeyeh@zoes-MacBook-Pro.local>
2022-05-06 20:24:28 +0800

    new version

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,52 +16,52 @@ from pydrive.drive import GoogleDrive
 
 
 db = firestore.Client.from_service_account_json("firestore-key.json")
-# # # Create a connection object.
-# # credentials = service_account.Credentials.from_service_account_info(
-# #     st.secrets["gcp_service_account"],
-# #     scopes=[
-# #         "https://www.googleapis.com/auth/spreadsheets",
-# #     ],
-# # )
-# # conn = connect(credentials=credentials)
-
-# # 網頁配置設定
-# st.set_page_config(
-#     page_title="Anita Mui 出道 40 週年應援活動",
-#     page_icon="random",
-#     layout="wide", # 網頁中佈局寬度，預設是"centered"，還可以使用"wide"
-#     initial_sidebar_state="collapsed", # 側邊欄顯示狀態，"expanded"打開或"collapsed"隱藏，預設是"auto"，代表在手機尺寸的設備上是隱藏，否則是打開顯示。
+# # Create a connection object.
+# credentials = service_account.Credentials.from_service_account_info(
+#     st.secrets["gcp_service_account"],
+#     scopes=[
+#         "https://www.googleapis.com/auth/spreadsheets",
+#     ],
 # )
+# conn = connect(credentials=credentials)
+
+# 網頁配置設定
+st.set_page_config(
+    page_title="Anita Mui 出道 40 週年應援活動",
+    page_icon="random",
+    layout="wide", # 網頁中佈局寬度，預設是"centered"，還可以使用"wide"
+    initial_sidebar_state="collapsed", # 側邊欄顯示狀態，"expanded"打開或"collapsed"隱藏，預設是"auto"，代表在手機尺寸的設備上是隱藏，否則是打開顯示。
+)
 
 
-# # Perform SQL query on the Google Sheet.
-# # Uses st.cache to only rerun when the query changes or after 10 min.
-# st.title('輸入你想對梅姐說的話：')
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+st.title('輸入你想對梅姐說的話：')
 
 
 
-# # Streamlit widgets to let a user create a new post
-# title = st.text_input("Post title")
-# url = st.text_input("Post url")
-# submit = st.button("Submit new post")
+# Streamlit widgets to let a user create a new post
+title = st.text_input("Post title")
+url = st.text_input("Post url")
+submit = st.button("Submit new post")
 
-# # Once the user has submitted, upload it to the database
-# if title and url and submit:
-# 	doc_ref = db.collection("posts").document(title)
-# 	doc_ref.set({
-# 		"title": title,
-# 		"url": url
-# 	})
+# Once the user has submitted, upload it to the database
+if title and url and submit:
+	doc_ref = db.collection("posts").document(title)
+	doc_ref.set({
+		"title": title,
+		"url": url
+	})
 
-# # And then render each post, using some light Markdown
-# posts_ref = db.collection("posts")
-# for doc in posts_ref.stream():
-# 	post = doc.to_dict()
-# 	title = post["title"]
-# 	url = post["url"]
+# And then render each post, using some light Markdown
+posts_ref = db.collection("posts")
+for doc in posts_ref.stream():
+	post = doc.to_dict()
+	title = post["title"]
+	url = post["url"]
 
-# 	st.subheader(f"Post: {title}")
-# 	st.write(f":link: [{url}]({url})")
+	st.subheader(f"Post: {title}")
+	st.write(f":link: [{url}]({url})")
 
 
 # # @st.cache(ttl=600)
