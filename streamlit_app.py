c85682bbe911248f613c1ef9fe43c0c51c8cd096
zoe yeh <zoeyeh@zoes-MacBook-Pro.local>
2022-05-07 00:25:02 +0800

    new version

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,7 @@ set_background('20211230mui_0.png')
 
 
 # Streamlit widgets to let a user create a new post
-nickname = st.text_input(1"暱稱:")
+nickname = st.text_input("1.暱稱:")
 place = st.text_input("2. 你是來自哪裡的粉絲:")
 years = st.text_input("3. 喜歡 Anita 已經有幾年了")
 sentence = st.text_input("4. 你想對 Anita 說")
