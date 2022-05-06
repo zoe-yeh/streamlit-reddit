f56b9aa8cacc182ee322d32d96dc63e964984bbb
zoe yeh <zoeyeh@zoes-MacBook-Pro.local>
2022-05-06 22:34:31 +0800

    new version

--- helper.py
+++ helper.py
@@ -34,87 +34,87 @@ def set_bg_hack():
          unsafe_allow_html=True
      )
         
-def header(text):
-    '''
-     A function to neatly display headers in app.
-    Parameters
-    ----------
-    text : Text to display as header
-    Returns
-    -------
-    A header defined by html5 code below.
-    '''
-    html_temp = f"""
-    <h2 style = "color:#F26531; text_align:center; font-weight: bold;"> {text} </h2>
-    </div>
-    """
+# def header(text):
+#     '''
+#      A function to neatly display headers in app.
+#     Parameters
+#     ----------
+#     text : Text to display as header
+#     Returns
+#     -------
+#     A header defined by html5 code below.
+#     '''
+#     html_temp = f"""
+#     <h2 style = "color:#F26531; text_align:center; font-weight: bold;"> {text} </h2>
+#     </div>
+#     """
     
-    st.markdown(html_temp, unsafe_allow_html = True)
+#     st.markdown(html_temp, unsafe_allow_html = True)
 
-def sub_text(text):
-    '''
-    A function to neatly display text in app.
-    Parameters
-    ----------
-    text : Just plain text.
-    Returns
-    -------
-    Text defined by html5 code below.
-    '''
+# def sub_text(text):
+#     '''
+#     A function to neatly display text in app.
+#     Parameters
+#     ----------
+#     text : Just plain text.
+#     Returns
+#     -------
+#     Text defined by html5 code below.
+#     '''
     
-    html_temp = f"""
-    <p style = "color:#1F4E79; text_align:justify;"> {text} </p>
-    </div>
-    """
+#     html_temp = f"""
+#     <p style = "color:#1F4E79; text_align:justify;"> {text} </p>
+#     </div>
+#     """
     
-    st.markdown(html_temp, unsafe_allow_html = True)
+#     st.markdown(html_temp, unsafe_allow_html = True)
     
 
-def expander():
-    '''
+# def expander():
+#     '''
     
-    Use Streamlit expander API and neatly show references.
-    Call sub_text function.
-    Returns
-    -------
-    An expander with special mentions and references.
-    '''
+#     Use Streamlit expander API and neatly show references.
+#     Call sub_text function.
+#     Returns
+#     -------
+#     An expander with special mentions and references.
+#     '''
     
-    expander_text = """
-        <p> Object detection functionality &#10024 </p>
-        <p> The model used is: <b>ssd_mobiledet_cpu_coco_int8.tflite</b>, downloaded from 
-        <a href= 'https://colab.research.google.com/github/sayakpaul/Adventures-in-TensorFlow-Lite/blob/master/MobileDet_Conversion_TFLite.ipynb#scrollTo=_rz1wbDv58t2' style="color:#F26531;" >this google colab</a>.
-        The author of this notebook is
-        <a href = 'https://github.com/sayakpaul' style="color:#F26531;" > sayakpaul </a>. </p>
-        <p> Snapshot functionality &#128247 </p>
-        <p> The webrtc snapshot functionality was shared in 
-        <a href = 'https://discuss.streamlit.io/t/new-component-streamlit-webrtc-a-new-way-to-deal-with-real-time-media-streams/8669/23?u=whitphx'  style="color:#F26531;" > this discussion </a> 
-        by the author of the component 
-        <a href = 'https://github.com/whitphx'  style="color:#F26531;" > whitphx </a>. </p> 
-        <p> Thanks so much to both authors and their amazing work &#129330 </p> 
-        """
+#     expander_text = """
+#         <p> Object detection functionality &#10024 </p>
+#         <p> The model used is: <b>ssd_mobiledet_cpu_coco_int8.tflite</b>, downloaded from 
+#         <a href= 'https://colab.research.google.com/github/sayakpaul/Adventures-in-TensorFlow-Lite/blob/master/MobileDet_Conversion_TFLite.ipynb#scrollTo=_rz1wbDv58t2' style="color:#F26531;" >this google colab</a>.
+#         The author of this notebook is
+#         <a href = 'https://github.com/sayakpaul' style="color:#F26531;" > sayakpaul </a>. </p>
+#         <p> Snapshot functionality &#128247 </p>
+#         <p> The webrtc snapshot functionality was shared in 
+#         <a href = 'https://discuss.streamlit.io/t/new-component-streamlit-webrtc-a-new-way-to-deal-with-real-time-media-streams/8669/23?u=whitphx'  style="color:#F26531;" > this discussion </a> 
+#         by the author of the component 
+#         <a href = 'https://github.com/whitphx'  style="color:#F26531;" > whitphx </a>. </p> 
+#         <p> Thanks so much to both authors and their amazing work &#129330 </p> 
+#         """
     
-    expander = st.beta_expander("References & special thanks", expanded=False)
+#     expander = st.beta_expander("References & special thanks", expanded=False)
     
-    with expander:
+#     with expander:
         
-        sub_text(expander_text)
+#         sub_text(expander_text)
 
-def write_image(out_image):
-    '''
+# def write_image(out_image):
+#     '''
     
-    Write image to tempDir folder with a unique name
+#     Write image to tempDir folder with a unique name
     
-    '''
+#     '''
     
-    today = date.today()
-    d = today.strftime("%b-%d-%Y")
+#     today = date.today()
+#     d = today.strftime("%b-%d-%Y")
     
-    t = time.localtime()
-    current_time = time.strftime("%H-%M-%S", t)
+#     t = time.localtime()
+#     current_time = time.strftime("%H-%M-%S", t)
     
-    file_name = "tempDir/photo_" + d + "_" + current_time + ".jpg"
+#     file_name = "tempDir/photo_" + d + "_" + current_time + ".jpg"
     
-    cv2.imwrite(file_name, out_image)
+#     cv2.imwrite(file_name, out_image)
     
-    return(file_name)
+#     return(file_name)
