c7d4fe561656f4c862d48b077a1e8095626ae317
zoe yeh <zoeyeh@zoes-MacBook-Pro.local>
2022-05-06 22:19:02 +0800

    new version

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ db = firestore.Client.from_service_account_json("firestore-key.json")
 
 
 # 網頁配置設定
-st.set_page_config(page_title="Anita")
+# st.set_page_config(page_title="Anita")
 
 # st.set_page_config(
 # 	page_title="Anita Mui 出道 40 週年應援活動", 
