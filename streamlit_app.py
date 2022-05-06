2a500f398c7ca3795e50c199291be066565181ef
zoe yeh <zoeyeh@zoes-MacBook-Pro.local>
2022-05-06 19:46:52 +0800

    add title

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ conn = connect(credentials=credentials)
 st.set_page_config(
     page_title="Anita Mui 出道 40 週年應援活動",
     page_icon="random",
-    layout="centered",
+    layout="wide", # 網頁中佈局寬度，預設是"centered"，還可以使用"wide"
     initial_sidebar_state="collapsed", # 側邊欄顯示狀態，"expanded"打開或"collapsed"隱藏，預設是"auto"，代表在手機尺寸的設備上是隱藏，否則是打開顯示。
 )
 
