2a1237119370d0730b41ac56c3064b713b956b04
zoe yeh <zoeyeh@zoes-MacBook-Pro.local>
2022-05-06 21:09:42 +0800

    new version

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ from pydrive.drive import GoogleDrive
 latest_iteration = st.empty()
 bar = st.progress(0)
 for i in range(100):
-	latest_iteration.text(f'目前進度 {i+1} %')
+	latest_iteration.text(f"目前進度 {i+1} %")
     bar.progress(i + 1)
     time.sleep(0.1)
 
