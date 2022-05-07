d2aa932292675cdcfacacbe729a8c3d4f6be2400
zoe yeh <zoeyeh@zoes-MacBook-Pro.local>
2022-05-07 08:25:38 +0800

    new version

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,12 +50,10 @@ st.markdown("<h2 style='text-align: center; color: black;'>未變情懷40載, �
 col1, col2 = st.columns(2)
 img = Image.open("./img/photo_2022-05-06_21-35-30.jpg")
 (w, h) = img.size
-st.head(int(w), int(h))
 new_w = int(w*0.4)
 new_h = int(h*0.4)
 
 new_img = img.resize((new_w, new_h))
-
 
 with col1:
     st.image(new_img)
