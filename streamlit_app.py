39d31c140189bfe1ef0d02a8f136923c27aa5cb9
zoe yeh <zoeyeh@zoes-MacBook-Pro.local>
2022-05-14 17:42:25 +0800

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,21 +4,30 @@ from google.cloud import firestore
 # Authenticate to Firestore with the JSON account key.
 db = firestore.Client.from_service_account_json("firestore-key.json")
 
-# Create a reference to the Google post.
-doc_ref = db.collection("posts").document("Google")
+# # Create a reference to the Google post.
+# doc_ref = db.collection("posts").document("Google")
 
-# Then get the data at that reference.
-doc = doc_ref.get()
+# # Then get the data at that reference.
+# doc = doc_ref.get()
 
-# Let's see what we got!
-st.write("The id is: ", doc.id)
-st.write("The contents are: ", doc.to_dict())
+# # Let's see what we got!
+# st.write("The id is: ", doc.id)
+# st.write("The contents are: ", doc.to_dict())
 
-# This time, we're creating a NEW post reference for Apple
-doc_ref = db.collection("posts").document("Apple")
+# # This time, we're creating a NEW post reference for Apple
+# doc_ref = db.collection("posts").document("Apple")
 
-# And then uploading some data to that reference
-doc_ref.set({
-	"title": "Apple",
-	"url": "www.apple.com"
-})
+# # And then uploading some data to that reference
+# doc_ref.set({
+# 	"title": "Apple",
+# 	"url": "www.apple.com"
+# })
+
+
+# Now let's make a reference to ALL of the posts
+posts_ref = db.collection("posts")
+
+# For a reference to a collection, we use .stream() instead of .get()
+for doc in posts_ref.stream():
+	st.write("The id is: ", doc.id)
+	st.write("The contents are: ", doc.to_dict())
