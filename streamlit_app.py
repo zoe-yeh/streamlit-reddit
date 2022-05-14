566fb6a6e110a9f1f01f5804b72e9faa567169bc
zoe yeh <zoeyeh@zoes-MacBook-Pro.local>
2022-05-14 17:41:18 +0800

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,12 @@ doc = doc_ref.get()
 # Let's see what we got!
 st.write("The id is: ", doc.id)
 st.write("The contents are: ", doc.to_dict())
+
+# This time, we're creating a NEW post reference for Apple
+doc_ref = db.collection("posts").document("Apple")
+
+# And then uploading some data to that reference
+doc_ref.set({
+	"title": "Apple",
+	"url": "www.apple.com"
+})
