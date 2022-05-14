367d6a663fe2125463842e2637ae4ef86f04337f
zoe yeh <zoeyeh@zoes-MacBook-Pro.local>
2022-05-14 17:43:08 +0800

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,17 @@
 import streamlit as st
 from google.cloud import firestore
 
-# Authenticate to Firestore with the JSON account key.
 db = firestore.Client.from_service_account_json("firestore-key.json")
 
-# # Create a reference to the Google post.
-# doc_ref = db.collection("posts").document("Google")
+# Streamlit widgets to let a user create a new post
+title = st.text_input("Post title")
+url = st.text_input("Post url")
+submit = st.button("Submit new post")
 
-# # Then get the data at that reference.
-# doc = doc_ref.get()
-
-# # Let's see what we got!
-# st.write("The id is: ", doc.id)
-# st.write("The contents are: ", doc.to_dict())
-
-# # This time, we're creating a NEW post reference for Apple
-# doc_ref = db.collection("posts").document("Apple")
-
-# # And then uploading some data to that reference
-# doc_ref.set({
-# 	"title": "Apple",
-# 	"url": "www.apple.com"
-# })
-
-
-# Now let's make a reference to ALL of the posts
-posts_ref = db.collection("posts")
-
-# For a reference to a collection, we use .stream() instead of .get()
-for doc in posts_ref.stream():
-	st.write("The id is: ", doc.id)
-	st.write("The contents are: ", doc.to_dict())
+# Once the user has submitted, upload it to the database
+if title and url and submit:
+	doc_ref = db.collection("posts").document(title)
+	doc_ref.set({
+		"title": title,
+		"url": url
+	})
