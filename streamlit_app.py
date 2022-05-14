b91aaee567eed31b4d72be32be0391db83119595
zoe yeh <zoeyeh@zoes-MacBook-Pro.local>
2022-05-14 17:54:02 +0800

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,20 +4,20 @@ from google.cloud import firestore
 # db = firestore.Client.from_service_account_json("firestore-key.json")
 import json
 key_dict = json.loads(st.secrets["textkey"])
-st.subheader(key_dict)
-# creds = service_account.Credentials.from_service_account_info(key_dict)
-# db = firestore.Client(credentials=creds, project="streamlit-reddit")
+# st.subheader(key_dict)
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds, project="streamlit-reddit")
 
-# # Streamlit widgets to let a user create a new post
-# title = st.text_input("Post title")
-# url = st.text_input("Post url")
-# submit = st.button("Submit new post")
+# Streamlit widgets to let a user create a new post
+title = st.text_input("Post title")
+url = st.text_input("Post url")
+submit = st.button("Submit new post")
 
-# # Once the user has submitted, upload it to the database
-# if title and url and submit:
-# 	doc_ref = db.collection("posts").document(title)
-# 	doc_ref.set({
-# 		"title": title,
-# 		"url": url
-# 	})
-# 	st.subheader(f"我們收到囉")
+# Once the user has submitted, upload it to the database
+if title and url and submit:
+	doc_ref = db.collection("posts").document(title)
+	doc_ref.set({
+		"title": title,
+		"url": url
+	})
+	st.subheader(f"我們收到囉")
